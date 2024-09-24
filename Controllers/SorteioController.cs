e1dcc9f59a708c46c1b8ad11f528f26e45dba9c0
LXPinho <149246830+LXPinho@users.noreply.github.com>
2024-09-24 06:44:39 -0300

    Ajustes

--- Controllers/SorteioController.cs
+++ Controllers/SorteioController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text;
+using System.Text.Json;
 using TestSwaggerSorteio.Data;
 using TestSwaggerSorteio.Models;
 
@@ -11,13 +13,16 @@ namespace TestSwaggerSorteio.Controllers
     public class SorteioController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<List<Sorteios>> ListarSorteior(int vibesAcumuladas = 300)
+        public ActionResult<string> ListarSorteior(int vibesAcumuladas = 0)
         {
+            string result = string.Empty;
             bool firstTime = true;
+            if (vibesAcumuladas <= 0) vibesAcumuladas = 300;
             SorteioData sorteioData = SorteioData.getInstance();
             for( int i = (new Random()).Next(1, 6); i > 0 && vibesAcumuladas / 30 > 1; i--, firstTime = false, vibesAcumuladas -= 30)
                  SorteioData.AddSorteioData(firstTime, vibesAcumuladas);
-            return Ok(sorteioData.getListSorteios());
+            result = JsonSerializer.Serialize(sorteioData.getListSorteios()) ;
+            return Ok(result);
         }
     }
 }
